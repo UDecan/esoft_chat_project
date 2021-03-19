298abc68e5282fabb16cc1d574e645aeb0678c59
Arthur <foparu68@gmail.com>
2021-03-19 22:13:39 +0500

    Навалил стилей. Добавил форму входа.

---
+++ front_chat/src/components/auth/Auth.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+    Button,
+    Input,
+    InputLabel,
+    FormControl,
+    Typography
+} from '@material-ui/core';
+import './Auth.scss';
+
+
+export default function Auth(props){
+    return(
+      <div className="auth_div blackShadow">
+        <Typography className="auth_title" variant="h5" color="primary">
+          Вход в систему
+        </Typography>
+        <FormControl>
+          <InputLabel htmlFor="user_name">
+            Логин
+          </InputLabel>
+          <Input
+            className="form_control"
+            type="text"
+            id="user_name"
+            placeholder="Ваш логин"
+            required
+          />
+        </FormControl>
+  
+        <FormControl>
+          <InputLabel htmlFor="user_password">
+            Пароль
+          </InputLabel>
+          <Input
+            className="form_control"
+            type="password"
+            id="user_password"
+            placeholder="Ваш пароль"
+          />
+        </FormControl>
+        <Button variant="contained" color="primary">
+          Войти
+        </Button>
+      </div>
+    );
+}
---
+++ front_chat/src/components/registr/Registr.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Registr(props){
+    return(
+        <div>
+            
+        </div>
+    );
+}
